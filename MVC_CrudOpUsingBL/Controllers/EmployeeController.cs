766a81c21019ab19c62d6d342f806b06a52e563b
Nandakumar Kadam <nandakumar.kadam@slksoft.com>
2018-09-04 11:02:45 +0530

    this is a try

---
+++ MVC_CrudOpUsingBL/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BussinessLayer;
+
+namespace MVC_CrudOpUsingBL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeBussinessLayer employeeBussinessLayer = new EmployeeBussinessLayer();
+            List<Employee> employees = employeeBussinessLayer.Employees.ToList();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public ActionResult create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult create(FormCollection formCollection)
+        {
+            Employee employee = new Employee();
+            employee.Name = formCollection["Name"];
+            employee.Gender = formCollection["Gender"];
+            employee.City = formCollection["City"];
+            employee.DateOfBirth = Convert.ToDateTime(formCollection["DateOfBirth"]);
+
+            EmployeeBussinessLayer employeeBussinessLayer = new EmployeeBussinessLayer();
+            employeeBussinessLayer.AddEmployee(employee);
+            return RedirectToAction("Index");
+        }
+    }
+}
